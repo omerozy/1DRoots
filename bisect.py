80cc334b05d3f1bde4410c742d041170453edb79
Omer Ozyilmaz <omerrozyilmaz@gmail.com>
2022-04-06 20:29:06 +0300

    Added algorithm file for bisection method.

---
+++ bisect.py
@@ -0,0 +1,37 @@
+import sys
+
+# Inputs
+xL = 3 # lower guess for root   # Note: Select lower and upper guesses such that xL < xR and f(xL)*f(xU) < 0
+xU = 9 # upper guess for root
+
+errLim = 10**-5 # solution error limit for stopping execution
+
+maxIterNum = 100 # maximum number of iterations
+
+def f(x):
+    y = x**2 - 15*x + 50 # define objective function here
+    return y
+
+# Check if conditions are met for execution
+if not xL < xU:
+    sys.exit("Method condition is not satisfied: Lower guess input shoule be lower than upper guess input.")
+elif not f(xL)*f(xU) < 0:
+    sys.exit("Method condition is not satisfied: Product of function outputs at two guesses shoule be less than 0.")
+
+# Execution of method
+solFound = False
+for iterIdx in range(maxIterNum):
+    xR = (xL + xU)/2 # estimate root as the middle of interval
+    if abs(f(xR)) < errLim:
+        xS = xR
+        solFound = True
+        break
+    elif f(xL)*f(xR) < 0: # root lies in lower subinterval
+        xU = xR
+    elif f(xL)*f(xR) > 0: # root lies in upper subinterval
+        xL = xR
+
+if solFound:
+    print("\nSolution was found as " + str(xS) + " after " + str(iterIdx + 1) + " iterations.\n")
+else:
+    print("Solution could not be found after specified number of iterations.")
